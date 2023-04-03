e35f08f642c83e3ca776a5fe983950bae4c442a8
jules319 <jules31901@gmail.com>
2023-04-03 16:43:21 -0400

    Filled out simple queries and added prompts for advanced queries

---
+++ src/sql_queries/WorksheetTwoAdvancedQueries.sql
@@ -0,0 +1,21 @@
+-- a. Provide a list of customer names, along with the total dollar amount each customer has spent.
+
+-- b. Provide a list of customer names and e-mail addresses for customers who have spent more than the
+-- average customer.
+
+-- c. Provide a list of the titles in the database and associated total copies sold to customers, sorted from the
+-- title that has sold the most individual copies to the title that has sold the least.
+
+-- d. Provide a list of the titles in the database and associated dollar totals for copies sold to customers,
+-- sorted from the title that has sold the highest dollar amount to the title that has sold the smallest.
+
+-- e. Find the most popular author in the database (i.e. the one who has sold the most books)
+
+-- f. Find the most profitable author in the database for this store (i.e. the one who has brought in the most
+-- money)
+
+-- g. Provide a list of customer information for customers who purchased anything written by the most
+-- profitable author in the database.
+
+-- h. Provide the list of authors who wrote the books purchased by the customers who have spent more than
+-- the average customer.
---
+++ src/sql_queries/WorksheetTwoSimpleQueries.sql
@@ -0,0 +1,49 @@
+-- a. Find the titles of all books by Pratchett that cost less than $10
+SELECT b.title
+FROM Books b
+JOIN BookAuthors ba ON b.isbn = ba.isbn
+JOIN Authors a ON ba.author_id = a.author_id
+WHERE a.name LIKE '%Pratchett%' AND b.price < 10;
+
+-- b. Give all the titles and their dates of purchase made by a single customer (you choose how to designate
+-- the customer)
+SELECT b.title, co.create_date
+FROM Books b
+JOIN Customer_Order_Invoice_Items coii ON b.isbn = coii.isbn_fk
+JOIN Customer_Order co ON coii.order_id_fk = co.order_id
+WHERE co.username_fk = 'customer_username';
+
+-- c. Find the titles and ISBNs for all books with less than 5 copies in stock
+SELECT b.title, b.isbn
+FROM Books b
+JOIN Warehouse_Inventory_Items wii ON b.isbn = wii.isbn_fk
+GROUP BY b.isbn
+HAVING SUM(wii.quantity) < 5;
+
+-- d. Give all the customers who purchased a book by Pratchett and the titles of Pratchett books they
+-- purchased
+SELECT c.username, b.title
+FROM Customer c
+JOIN Customer_Order co ON c.username = co.username_fk
+JOIN Customer_Order_Invoice_Items coii ON co.order_id = coii.order_id_fk
+JOIN Books b ON coii.isbn_fk = b.isbn
+JOIN BookAuthors ba ON b.isbn = ba.isbn
+JOIN Authors a ON ba.author_id = a.author_id
+WHERE a.name LIKE '%Pratchett%';
+
+-- e. Find the total number of books purchased by a single customer (you choose how to designate the
+-- customer)
+SELECT co.username_fk, COUNT(*) as total_books_purchased
+FROM Customer_Order co
+JOIN Customer_Order_Invoice_Items coii ON co.order_id = coii.order_id_fk
+WHERE co.username_fk = 'customer_username'
+GROUP BY co.username_fk;
+
+-- f. Find the customer who has purchased the most books and the total number of books they have
+-- purchased
+SELECT co.username_fk, COUNT(*) as total_books_purchased
+FROM Customer_Order co
+JOIN Customer_Order_Invoice_Items coii ON co.order_id = coii.order_id_fk
+GROUP BY co.username_fk
+ORDER BY total_books_purchased DESC
+LIMIT 1;
